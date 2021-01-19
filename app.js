a86e9b87c88dc95b167d8ceb8bc60b086742edb8
evannor <erica.vannorman@gmail.com>
2021-01-19 10:07:40 -0800

    Array methods, reduce example

--- app.js
+++ app.js
@@ -30,22 +30,34 @@
 
 // Data stored in Arrays
 // can be similar and diverse
-const hobbies = ["cooking", "sports", "uniform data"];
-const userData = [
-  32,
-  "Erica",
-  { placesLived: ["New York", "Arizona", "Georgia"] },
-];
+// const hobbies = ["cooking", "sports", "uniform data"];
+// const userData = [
+//   32,
+//   "Erica",
+//   { placesLived: ["New York", "Arizona", "Georgia"] },
+// ];
 
 // Mulitdimensional arrays
-const analyticalData = [
-  [1.8, 3],
-  [7.4, 1],
-  [2.3, 2],
-];
+// const analyticalData = [
+//   [1.8, 3],
+//   [7.4, 1],
+//   [2.3, 2],
+// ];
 // below code with not work on array [[1.5, 4], 5]
-for (const data of analyticalData) {
-  for (const dataPoint of data) {
-    console.log(dataPoint);
-  }
-}
+// for (const data of analyticalData) {
+//   for (const dataPoint of data) {
+//     console.log(dataPoint);
+//   }
+// }
+
+// push(), pop(), unshift(), shift(), splice(), slice(), concat(), indexOf(), lastIndexOf(), find(), findIndex(), includes(), forEach(), map(), sort(), reverse(), filter(), reduce()
+
+const prices = [10.99, 5.99, 3.99, 6.59];
+
+// take array and 'reduces' it in a specified way to a single value
+// param after function call is the starting value (ie sum = 0, and then we add to it) of prevValue
+// currentValue is value at a specific index
+const sum = prices.reduce((prevValue, currentValue) => {
+  return prevValue + currentValue;
+}, 0);
+console.log(sum);
